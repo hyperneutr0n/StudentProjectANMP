d79d1c8e1cb07333b8b934efe7bc12f805a12c02
hyperneutr0n <randylemuel21@gmail.com>
2025-09-22 10:20:10 +0700

    result

--- app/src/main/java/com/neutron/studentproject/view/StudentListFragment.kt
+++ app/src/main/java/com/neutron/studentproject/view/StudentListFragment.kt
@@ -1,14 +1,13 @@
 package com.neutron.studentproject.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.neutron.studentproject.R
 import com.neutron.studentproject.databinding.FragmentStudentListBinding
 import com.neutron.studentproject.viewmodel.ListViewModel
 
@@ -33,7 +32,7 @@ class StudentListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel = ViewModelProvider(this)[ListViewModel::class.java]
         viewModel.refresh()
 
         binding.recView.layoutManager = LinearLayoutManager(context)
@@ -45,6 +44,23 @@ class StudentListFragment : Fragment() {
     fun observeViewModel() {
         viewModel.studentsLD.observe(viewLifecycleOwner, Observer {
             studentListAdapter.updateStudentList(it)
+        })
+
+        viewModel.studentLoadErrorLD.observe(viewLifecycleOwner, Observer {
+            if (it == true)
+                binding.txtError.visibility = View.VISIBLE
+            else
+                binding.txtError.visibility = View.GONE
+        })
+
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if (it == true) {
+                binding.recView.visibility = View.GONE
+                binding.progressLoad.visibility = View.VISIBLE
+            } else {
+                binding.recView.visibility = View.VISIBLE
+                binding.progressLoad.visibility = View.GONE
+            }
         })
     }
 }
